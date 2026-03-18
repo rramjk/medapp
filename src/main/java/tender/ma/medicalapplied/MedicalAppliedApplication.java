4ab170c73dc0b11c03376ea034c5b1f3bada9848
Рушан Рамазанов <rrramazanov@lanit.ru>
2026-03-18 23:10:42 +0400

    MED-20 init commit

---
+++ src/main/java/tender/ma/medicalapplied/MedicalAppliedApplication.java
@@ -0,0 +1,13 @@
+package tender.ma.medicalapplied;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppliedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppliedApplication.class, args);
+	}
+
+}
